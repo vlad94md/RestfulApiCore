a3258a323d8e93f48ea68973d3d5016d424807b7
Vladislav Guleaev <guleaev.vlad@gmail.com>
2017-04-19 09:54:15 +0300

    Added mapping

--- Library/src/Library.API/Controllers/AuthorsController.cs
+++ Library/src/Library.API/Controllers/AuthorsController.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.API.Helpers;
+using Library.API.Models;
 using Library.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.API.Controllers
 {
+    [Route("/api/authors")]
     public class AuthorsController : Controller
     {
         private ILibraryRepository repository;
@@ -16,9 +19,23 @@ namespace Library.API.Controllers
             this.repository = repository;
         }
 
+        [HttpGet()]
         public IActionResult GetAuthors()
         {
-            var authors = repository.GetAuthors();
+            var authorsEntities = repository.GetAuthors();
+
+            var authors = new List<AuthorDto>();
+
+            foreach (var author in authorsEntities)
+            {
+                authors.Add(new AuthorDto()
+                {
+                    Id = author.Id,
+                    Name = $"{author.FirstName} {author.LastName}",
+                    Genre = author.Genre,
+                    Age = author.DateOfBirth.GetCurrentAge()
+                });
+            }
 
             return new JsonResult(authors);
         }
